618e3a6db3131034f84d07a6c90c01e969e60558
unknown <yjs12180825@gmail.com>
2024-03-31 01:11:48 +0900

    :tada: Digitech.in 프로젝트 시작 :tada:

---
+++ src/main/java/com/mc/digitechin/digitechin/DigitechInApplication.java
@@ -0,0 +1,13 @@
+package com.mc.digitechin.digitechin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitechInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitechInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mc/digitechin/digitechin/DigitechInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.digitechin.digitechin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitechInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
