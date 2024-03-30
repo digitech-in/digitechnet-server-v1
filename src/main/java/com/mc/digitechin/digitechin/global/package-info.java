fe2b693c64e58a8289c307fa539fcd0f74fd97eb
unknown <yjs12180825@gmail.com>
2024-03-31 01:28:12 +0900

    :memo: global 패키지 생성 :memo:

---
+++ src/main/java/com/mc/digitechin/digitechin/global/package-info.java
@@ -0,0 +1,11 @@
+package com.mc.digitechin.digitechin.global;
+/**
+ * 해당 패키지는 공용으로 사용되는 객체들을 다루기 위한 global 패키지입니다
+ * <p>
+ * 해당 패키지는 auth, common, error 패키지를 하위 패키지로 가지고 있습니다
+ * </p>
+ *
+ * @since 1.0
+ * @author yunjisang
+ * @version 1.0
+ */
