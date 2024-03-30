a48f3f729ec5d03409ed92c4f25d3a6be233b7cb
unknown <yjs12180825@gmail.com>
2024-03-31 01:17:40 +0900

    :memo: domain/User 패키지 생성 :memo:

---
+++ src/main/java/com/mc/digitechin/digitechin/domain/User/package-info.java
@@ -0,0 +1,11 @@
+package com.mc.digitechin.digitechin.domain.User;
+/**
+ * 해당 패키지는 사용자를 관리하기 위한 User 패키지입니다
+ * <p>
+ * 해당 패키지는 유저 데이터들을 등록, 수정, 관리하기 위한 컨트롤러와 DTO, 서비스, 레포지토리 등을 하위 패키지로 가지고 있습니다.
+ * </p>
+ *
+ * @since 1.0
+ * @author yunjisang
+ * @version 1.0
+ */
